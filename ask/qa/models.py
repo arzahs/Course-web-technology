85569ce4a156b9bb3e92b7463f9984da08937b18
Sergey Nelepa <nelepa1995@mail.ru>
2016-03-19 12:50:18 +0300

    add model Qustion and Answer

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    author = models.ForeignKey(User)
+
+
+class QuestionStatus(models.Model):
+    RATING_CHOISES = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5)
+    )
+    rating = models.IntegerField(choices=RATING_CHOISES)
+    likes = models.IntegerField()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User)
+
+
+
+#class Author(models.Model):
+#class Like(models.Model):
