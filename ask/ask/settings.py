79eee6c31626ca7a525c4675d1a161b552a59811
Sergey Nelepa <nelepa1995@mail.ru>
2016-03-21 10:01:15 +0300

    edit templates askform

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.mysql',
        'NAME': 'qa',
        'USER': 'root',
-       'PASSWORD': '',
+       'PASSWORD': 'root',
        'HOST': 'localhost',
        'PORT': '3306',
    }
